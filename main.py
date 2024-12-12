bf83d90092d0c309ab4b4b7a5035557566bae99e
sirvx <Artem.Kravchuk@infiz.khpi.edu.ua>
2024-12-12 11:55:02 +0200

    random jump

---
+++ main.py
@@ -0,0 +1,165 @@
+from pygame import *
+import time as t
+import random
+
+init()
+font.init()
+
+size = 1200, 800
+window = display.set_mode(size)
+clock = time.Clock()
+
+ft = font.Font(None, 50)
+
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, img, x, y, width, height):
+        super().__init__()
+        self.image = transform.scale(image.load(img), (width, height))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.score = 0
+
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+class Birds(GameSprite):
+    def __init__(self, img, x, y, width, height):
+        super().__init__(img, x, y, width, height)
+        self.jump = False
+        self.y_vel = 0.2
+        self.fall_speed = 4
+        self.died = False
+
+    def update(self):
+
+        if self.jump:
+            self.rect.y -= 20
+            self.jump = False
+            self.fall_speed = 4
+        else:
+            self.rect.y += self.fall_speed
+            self.fall_speed += self.y_vel
+
+        if self.rect.y <= 0 or self.rect.y >= size[1]:
+            self.died = True
+
+    def fall_bird(self):
+        self.rect.y += self.fall_speed
+        self.fall_speed += self.y_vel
+
+
+class Pipe(GameSprite):
+    def __init__(self, img, x, y, width, height, reverse=False):
+        super().__init__(img, x, y, width, height)
+        self.speed = 8
+        self.reverse = reverse
+        self.passed = False
+        if self.reverse:
+            self.image = transform.rotate(self.image, 180)
+
+    def update(self):
+        self.rect.x -= self.speed
+        if self.rect.x <= - 150:
+            self.kill()
+        if self.rect.x <= bird.rect.x and not self.passed:
+            self.passed = True
+            bird.score += 0.5
+
+        if len(pipes) < 6:
+            create_new_pipe()
+
+
+def new_birds():
+    bird = Birds('img/bird.png', 100, 300, 80, 80)
+    return bird
+
+pipes = sprite.Group()
+
+
+def create_new_pipe():
+    x = 1500
+    for i in range(10):
+        y1 = random.randint(100, 400)
+        pipe = Pipe('img/pipe.png', x, size[1] - y1, 150, 400)
+        pipes.add(pipe)
+
+        y2 = y1 - random.randint(80, 120)
+        pipe = Pipe('img/pipe.png', x, 0 - y2, 150, 400, True)
+        pipes.add(pipe)
+        x += random.randint(500, 700)
+
+
+
+
+score = 0
+create_new_pipe()
+bird =new_birds()
+high_score = 0
+'''data = {
+    "bird_y":bird.rect.y,
+    "bird_x": 100,
+    "bird_speed": bird.fall_speed,
+    "pipe_gap_y": list(pipes)[0].rect.y - list(pipes)[1].rect.y + 400,  # розрив між трубами
+    "bird_to_pipe_gap_y": bird.rect.y - (list(pipes)[0].rect.y + list(pipes)[1].rect.y + 400) / 2,  # положення пташки відносно проміжку
+
+}'''
+
+data = []
+def save_data(data):
+    df = pd.DataFrame(data)
+    df.to_csv('output.csv', index=False)
+
+while True:
+    for e in event.get():
+        if e.type == QUIT:
+            quit()
+        if e.type == KEYDOWN:
+            if e.key == K_ESCAPE:
+                quit()
+    window.fill((100, 200, 200))
+    bird.reset()
+    pipes.draw(window)
+
+    score_text = ft.render('Рахунок: ' + str(bird.score), True, (255, 255, 255))
+    window.blit(score_text, (600, 50))
+
+
+    if bird.died:
+        bird.fall_bird()
+
+    display.update()
+    clock.tick(60)
+
+    if sprite.spritecollide(bird, pipes, False) and not bird.died:
+        bird.died = True
+    else:
+        should_jump = random.choice([True, False, False, False])
+        data.append({
+            "bird_y": bird.rect.y,
+            "bird_x": 100,
+            "bird_speed": bird.fall_speed,
+            "pipe_down_x": list(pipes)[0].rect.x,
+            "pipe_down_y": list(pipes)[0].rect.y,
+            "pipe_up_x": list(pipes)[1].rect.x,
+            "pipe_up_y": list(pipes)[1].rect.y,
+            "pipe_gap_y": list(pipes)[0].rect.y - list(pipes)[1].rect.y + 400,
+            "bird_to_pipe_gap_y": bird.rect.y - (list(pipes)[0].rect.y + list(pipes)[1].rect.y + 400) / 2
+        })
+
+        bird.jump = should_jump
+
+
+    bird.update()
+    pipes.update()
+
+    if bird.died:
+        t.sleep(0.5)
+        score = 0
+        pipes = sprite.Group()
+        create_new_pipe()
+        bird = new_birds()
+        bird.died = False
